05d0ed52720ebdfb99ead2428f48820c0fc491cd
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-08-01 10:12:03 +0200

    chcuknorris

---
+++ src/compenents/Api.jsx
@@ -0,0 +1,6 @@
+
+
+const ApiRandom = "https://api.chucknorris.io/jokes/random";
+
+
+export default Api;
---
+++ src/compenents/ChuckNorris.jsx
@@ -0,0 +1,9 @@
+import "../scss/style.css";
+import Api from "./Api";
+
+
+
+
+
+
+export default ChuckNorris;
